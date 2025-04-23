3e45166eb2035a1418721b2f2c0e4e5ed650b213
Griso <luca.grisoni@polimi.it>
2025-04-23 16:17:45 +0200

    risolto problema plot matrice risposta e commentatoil debugging

--- response_matrix.py
+++ response_matrix.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 def response_matrix(response_file,counts_file,energy_file):
-    st.write("✅ Caricamento della matrice di risposta...")
+    #st.write("✅ Caricamento della matrice di risposta...")
     R = np.loadtxt(response_file)
 
-    st.write("✅ Trasposizione della matrice...")
+    #st.write("✅ Trasposizione della matrice...")
     R = R.T
     funz = [0,1,2,3,4,5,6,7,8,9]
     stop = len(funz)
     
-    st.write("✅ Selezione dei detector:", funz)
+    #st.write("✅ Selezione dei detector:", funz)
     R = R[funz, :]
     
-    st.write("✅ Caricamento dei conteggi...")
+    #st.write("✅ Caricamento dei conteggi...")
     data_full = np.loadtxt(counts_file)
     data = data_full[funz]
     
-    st.write("✅ Caricamento delle energie...")
+    #st.write("✅ Caricamento delle energie...")
     energies = np.loadtxt(energy_file)
     
     if energies.shape[1] < 3:
@@ -27,7 +27,7 @@ def response_matrix(response_file,counts_file,energy_file):
     
     E_new = energies[:, 2]  # bin centrali
     
-    st.write("✅ Plotting...")
+    #st.write("✅ Plotting...")
     for i in range(stop):
         plt.plot(E_new, R[i, :], marker='o', linestyle='-', label=f"Scint {i+1}")
         
@@ -38,7 +38,7 @@ def response_matrix(response_file,counts_file,energy_file):
         plt.grid(True, which="both", linestyle="--", linewidth=0.5)
         plt.axvline(x=0.5e-6, color='r', linestyle='--', label="0.5")
         
-        st.pyplot(plt.gcf())  # Mostra il grafico nella webapp
+    st.pyplot(plt.gcf())  # Mostra il grafico nella webapp
 
     st.success("✅ Matrice e dati caricati con successo.")
     

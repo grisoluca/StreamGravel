6296b38ec8955d69d02370c66c04ca0b45463537
Griso <luca.grisoni@polimi.it>
2025-06-19 10:03:48 +0200

    estremi constant

--- StreamGravel.py
+++ StreamGravel.py
@@ -17,7 +17,7 @@ st.set_page_config(
 
 #st.set_page_config(layout="wide")
 st.title("Neutron Spectrum Unfolding")
-st.title("figi")
+st.title("figccci")
 
 # --------------------- SIDEBAR (Controlli) ---------------------
 st.sidebar.header("⚙️ Unfolding parameters")
